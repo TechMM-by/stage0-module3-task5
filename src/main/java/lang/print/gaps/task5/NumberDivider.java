a12d83e037f15385d465d6b54b4df4b0f250af08
Maria Maksimova <drogzed@gmail.com>
2023-10-26 21:53:14 +0400

    first

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double result;
+        result = (toBeDivided * 1.0) / 5;
+
+        System.out.println(result);
     }
 }
